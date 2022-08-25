b5cbd9e19bab5471a1c5b9338a2bb5e6b9148a01
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:21:41 +0530

    *Check User Mobile Number Is Valid Or Not *

---
+++ UserMobileNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+  public  class UserMobileNumber
+    {
+        string Mobile= "^[91]{2}[\\s][6-9]{1}[0-9]{9}$";
+
+
+        public string ValidMobilenumberCheckusingLambda(string mobilenumber) => Regex.IsMatch(mobilenumber, Mobile) ? "Mobile Number  is Valid" : "Mobile Number is Not Valid";
+    }
+}
