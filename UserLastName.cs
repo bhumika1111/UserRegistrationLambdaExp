a671a7916187370ac981abc62d911df4be101a6a
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:17:34 +0530

    *Check User Last Name Is Valid Or Not *

---
+++ UserLastName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    class UserLastName
+    {
+        string Lastname = "^[A-Z]{1}[a-z]{2,7}$";
+
+
+        public string LastnameCheckusingLambda(string lastname) => Regex.IsMatch(lastname, Lastname) ? "LastName is Valid" : "LastName is Not Valid";
+    }
+}
