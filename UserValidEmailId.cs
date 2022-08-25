688f55b31ccdc547334de189d35417419df7febc
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:28:59 +0530

    *Check User Email  Is Valid Or Not *

---
+++ UserValidEmailId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    class UserValidEmailId
+    {
+        string EmailId = "^[a-z]{3,5}['@'][a-z]{3,7}['.'][a-z]{3}$";
+
+
+        public string ValidEmailCheckusingLambda(string email) => Regex.IsMatch(email, EmailId) ? "Email is Valid" : "Email is Not Valid";
+    }
+}
+
