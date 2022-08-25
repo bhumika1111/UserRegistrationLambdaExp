fdc447ac5d41ff4704b5e9e606952c6b77817410
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:26:09 +0530

    *Check User Password For Rule3  Is Valid Or Not *

---
+++ UserPasswordRule3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    class UserPasswordRule3
+    {
+        string UserPass3 = "^[A-Z]{1}[0-9]{1}[A-Z a-z]{7}$";
+
+
+        public string ValidPasswordCheckusingLambda(string password) => Regex.IsMatch(password, UserPass3) ? "Password is Valid" : "Password is Not Valid";
+    }
+}
