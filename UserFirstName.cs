4b64d52336cd7e3aa2abcf1e30b5b87aa0e06382
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:15:55 +0530

    *Check User First Name Is Valid Or Not *

---
+++ UserFirstName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+   public class UserFirstName
+    {
+        string Firstname = "^[A-Z]{1}[a-z]{2,7}$";
+
+
+        public string FirstnameCheckusingLambda(string firstname) => Regex.IsMatch(firstname, Firstname) ? "FirstName is Valid" : "FirstName is Not Valid";
+       
+    }
+}
