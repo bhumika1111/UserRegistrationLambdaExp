ad823d9291710a67ae6b9b84aa522d7f3cebff0d
bhumika1111 <bhoyarpatil@434>
2022-08-25 23:23:47 +0530

    *Check User Password For Rule1  Is Valid Or Not *

---
+++ UserPasswordRule1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationUsingLambda
+{
+    class UserPasswordRule1
+    {
+        string UserPass1 = "^[A-Z]{1}[A-z a-z]{7,15}$";
+
+        public string ValidPasswordCheckusingLambda(string password) => Regex.IsMatch(password, UserPass1) ? "Password is Valid" : "Password is Not Valid";
+    }
+}
